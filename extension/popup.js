ae295e313acacc3d4113948e5db2a420b92f032b
bheftye <bheftye92@gmail.com>
2015-06-22 13:02:02 -0500

    Screenshot y guardado de imagen.

--- extension/popup.js
+++ extension/popup.js
@@ -17,6 +17,7 @@ function getSummonerInfo(){
         if (xmlhttp.readyState==4 && xmlhttp.status==200)
           {
           document.getElementById("elemento").innerHTML= JSON.parse(xmlhttp.responseText);
+          $("#share_button").removeClass("hidden-xs");
           }
         }
       //var params = "summ="+$("#summname").val()+"&serv="+$("#server").val();
@@ -36,3 +37,107 @@ document.addEventListener('DOMContentLoaded', function() {
         getSummonerInfo();
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var submit_button = document.getElementById('share_button');
+    // onClick's logic below:
+    submit_button.addEventListener('click', function() {
+        shareSearch();
+    });
+});
+
+function shareSearch(){
+  var blob = generate();
+  var base64data;
+
+  var reader = new window.FileReader();
+  reader.readAsDataURL(blob); 
+  reader.onloadend = function() {
+    base64data = reader.result;                
+  }
+
+  var xmlhttp;
+  if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  }
+  else{// code for IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function(){
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      console.log(JSON.parse(xmlhttp.responseText);
+)
+    }
+  }
+
+  xmlhttp.open("POST","http://localhost:8888/lolextension/servidor/img_handler.php",true);
+  xmlhttp.send(base64data);
+
+}
+
+(function (exports) {
+    function urlsToAbsolute(nodeList) {
+        if (!nodeList.length) {
+            return [];
+        }
+        var attrName = 'href';
+        if (nodeList[0].__proto__ === HTMLImageElement.prototype 
+        || nodeList[0].__proto__ === HTMLScriptElement.prototype) {
+            attrName = 'src';
+        }
+        nodeList = [].map.call(nodeList, function (el, i) {
+            var attr = el.getAttribute(attrName);
+            if (!attr) {
+                return;
+            }
+            var absURL = /^(https?|data):/i.test(attr);
+            if (absURL) {
+                return el;
+            } else {
+                return el;
+            }
+        });
+        return nodeList;
+    }
+
+    function screenshotPage() {
+        urlsToAbsolute(document.images);
+        urlsToAbsolute(document.querySelectorAll("link[rel='stylesheet']"));
+        var screenshot = document.documentElement.cloneNode(true);
+        var b = document.createElement('base');
+        b.href = document.location.protocol + '//' + location.host;
+        var head = screenshot.querySelector('head');
+        head.insertBefore(b, head.firstChild);
+        screenshot.style.pointerEvents = 'none';
+        screenshot.style.overflow = 'hidden';
+        screenshot.style.webkitUserSelect = 'none';
+        screenshot.style.mozUserSelect = 'none';
+        screenshot.style.msUserSelect = 'none';
+        screenshot.style.oUserSelect = 'none';
+        screenshot.style.userSelect = 'none';
+        screenshot.dataset.scrollX = window.scrollX;
+        screenshot.dataset.scrollY = window.scrollY;
+        var script = document.createElement('script');
+        script.textContent = '(' + addOnPageLoad_.toString() + ')();';
+        screenshot.querySelector('body').appendChild(script);
+        var blob = new Blob([screenshot.outerHTML], {
+            type: 'text/html'
+        });
+        return blob;
+    }
+
+    function addOnPageLoad_() {
+        window.addEventListener('DOMContentLoaded', function (e) {
+            var scrollX = document.documentElement.dataset.scrollX || 0;
+            var scrollY = document.documentElement.dataset.scrollY || 0;
+            window.scrollTo(scrollX, scrollY);
+        });
+    }
+
+    function generate() {
+       return screenshotPage();
+    }
+    exports.screenshotPage = screenshotPage;
+    exports.generate = generate;
+})(window);
+
---
+++ servidor/img_handler.php
@@ -0,0 +1,10 @@
+<?php
+	define('UPLOAD_DIR', 'images/');
+	$img = $_POST['base64data'];
+	$img = str_replace('data:image/png;base64,', '', $img);
+	$img = str_replace(' ', '+', $img);
+	$data = base64_decode($img);
+	$file = UPLOAD_DIR . uniqid() . '.png';
+	$success = file_put_contents($file, $data);
+	echo $success ? json_encode($file) : json_encode('Unable to save the file.');
+?>
