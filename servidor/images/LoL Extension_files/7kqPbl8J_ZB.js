7aac546964a8560916fa3a6616a342efcdc3eb1d
bheftye <bheftye92@gmail.com>
2015-06-23 23:25:28 -0500

    Cambios de ultima hora

---
+++ servidor/images/LoL Extension_files/7kqPbl8J_ZB.js
@@ -0,0 +1,5 @@
+/*!CK:4122678492!*//*1433859943,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["\/tFmd"]); }
+
+__d("DeveloperAppSwitchButton",["DOMEventListener","emptyFunction"],function(a,b,c,d,e,f,g,h){b.__markCompiled&&b.__markCompiled();function i(j){"use strict";this.$DeveloperAppSwitchButton0=j.checked;this.$DeveloperAppSwitchButton1=h;g.add(j,'change',function(k){this.$DeveloperAppSwitchButton0=j.checked;this.$DeveloperAppSwitchButton1(this);}.bind(this));}i.prototype.setOnChange=function(j){"use strict";this.$DeveloperAppSwitchButton1=j;};i.prototype.isChecked=function(){"use strict";return this.$DeveloperAppSwitchButton0;};e.exports=i;},null);
--- servidor/index.php
+++ servidor/index.php
@@ -4,13 +4,15 @@
 	$summoner_name = $_REQUEST["summ"]; //nombre de usuario a buscar.
 	$server = strtolower($_REQUEST["serv"]); //tipo de operación a realizar.
 	$api_key = "42fafbb1-0736-4c03-add6-21c3c4f18830";
+	$servers = array("eune" => "EUN1", "euw" => "EUW1", "kr" => "KR1", "ru" => "RU", "tr" => "TR1", "lan" => "LA1", "na" => "NA1", "br" => "BR1", "las" => "LA2", "oce" => "OC1");
+
 	$finalJson = "";
 	if($summoner_name != "" && $server != ""){
 		$jsonurl = "https://lan.api.pvp.net/api/lol/".$server."/v1.4/summoner/by-name/".$summoner_name."?api_key=".$api_key;
 		$json = file_get_contents($jsonurl);
 		$summoner_array = json_decode($json, true);
 		$summoner_id = $summoner_array[$summoner_name]["id"];
-		$servers = array("eune" => "EUN1", "euw" => "EUW1", "kr" => "KR1", "ru" => "RU", "tr" => "TR1", "lan" => "LA1", "na" => "NA1", "br" => "BR1", "las" => "LA2", "oce" => "OC1");
+
 		$jsonurl2 = "https://lan.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/".$servers[$server]."/".$summoner_id."?api_key=".$api_key;
 		$json2 = file_get_contents($jsonurl2);
 		$game_array = json_decode($json2, true);
@@ -80,11 +82,11 @@
 							</td>
 							<td>
 								<div style='margin:0 auto;display:table;'>
-									<img src='images/".$Champion -> championName.".png ' style='width:30;height:30px;clear:none;float:left;'>
+									<img src='images/".$Champion -> championName.".png ' class='champion-icon' style='width:30;height:30px;clear:none;float:left;'>
 									<div style='height:30px;width:15px;clear:none;float:left;position:relative;'>
-										<img src='images/".$SummonerSpell1 -> summonerSpellName.".png' style='position:absolute;width:14px;height:14px;top:0;left:2px;'>
+										<img src='images/".$SummonerSpell1 -> summonerSpellName.".png' class='spell-icon-1' style='position:absolute;width:14px;height:14px;top:0;left:2px;'>
 										<br>
-										<img src='images/".$SummonerSpell2 -> summonerSpellName.".png' style='position:absolute;width:14px;height:14px;bottom:0;left:2px;'>
+										<img src='images/".$SummonerSpell2 -> summonerSpellName.".png' class='spell-icon-2' style='position:absolute;width:14px;height:14px;bottom:0;left:2px;'>
 									</div>
 								</div>
 							</td>
